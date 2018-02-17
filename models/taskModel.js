4e2c58f17d3df30b8953f2485b6f61bc80246db8
Michell Alegría <alegria_trm@hotmail.com>
2018-02-16 23:32:08 -0600

    task model validations

--- models/taskModel.js
+++ models/taskModel.js
@@ -4,11 +4,11 @@ const Schema = mongoose.Schema;
 
 //model definition
 const taskSchema = new Schema({
-    description: String,
-    elapsed: Number,
-    index: Number,
-    status: Number,
-    totalDuration: Number
+    description: { type: String, default: "" },
+    elapsed: { type: Number, default: 0 },
+    index: { type: Number, required: true },
+    status: { type: Number, default: 0 },
+    duration: { type: Number, default: 0 }
 });
 
 //model class
